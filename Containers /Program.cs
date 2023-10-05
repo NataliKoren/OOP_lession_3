5bb1b19af893e0ac343f3e84eebe354a812513ca
Natali <129494640+NataliKoren@users.noreply.github.com>
2023-10-05 12:50:57 +0300

    Program.cs

---
+++ Containers /Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dotNetProgrammers.Containers;
+
+namespace dotNetProgrammers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string FILE_NAME = @"D:\employees.xml";
+            if (File.Exists(FILE_NAME))
+            {
+                var employeeContainer = new EmployeeContainer();
+                try
+                {
+                    employeeContainer.LoadXmlFile(FILE_NAME);
+                    Console.WriteLine("\na)\n");
+                    employeeContainer.SortedEmployees();
+                    Console.WriteLine("\nb)\n");
+                    employeeContainer.GetFirstNEmployees(5);
+                    Console.WriteLine("\nc)\n");
+                    employeeContainer.GetLastNEmployees(3);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Can't found file");
+            }
+            Console.ReadLine();
+        }
+    }
+}

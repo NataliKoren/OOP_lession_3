2bfb1c26d9142f138a05db60bb97a164b9f7f21e
Natali <129494640+NataliKoren@users.noreply.github.com>
2023-10-05 12:48:41 +0300

    FixedSalaryEmployee.cs

---
+++ Containers /Models /FixedSalaryEmployee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNetProgrammers.Models
+{
+    class FixedSalaryEmployee:Employee
+    {
+        public FixedSalaryEmployee(int id, string name, double salary) : base(id, name, salary)
+        {
+            
+        }
+        public override double GetSalary()
+        {
+            return Salary;
+        }
+    }
+}

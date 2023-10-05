86bac0c8a7a198f743bf47cb1d86c630b0330a89
Natali <129494640+NataliKoren@users.noreply.github.com>
2023-10-05 12:49:12 +0300

    HourlySalaryEmployee.cs

---
+++ Containers /Models /HourlySalaryEmployee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNetProgrammers.Models
+{
+    class HourlySalaryEmployee:Employee
+    {
+        public HourlySalaryEmployee(int id, string name, double salary) : base(id, name, salary)
+        {
+            
+        }
+        public override double GetSalary()
+        {
+            return 20.8 * 8 * Salary;
+        }
+    }
+}

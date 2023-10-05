90110082ea7ef9f9c80b7e81f2cb3726050add1f
Natali <129494640+NataliKoren@users.noreply.github.com>
2023-10-05 12:48:09 +0300

    Employee.cs

---
+++ Containers /Models /Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNetProgrammers.Models
+{
+    abstract class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        protected Employee()
+        {
+            
+        }
+
+        protected Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public abstract double GetSalary();
+
+        public override string ToString()
+        {
+            return $"{Name} = {GetSalary()}";
+        }
+    }
+}
